7ca6378fdc777badb461b13783766908014c112f
Valenti <valendieguez89@gmail.com>
2019-03-11 17:39:17 +0100

    funciona shoe

---
+++ main.js
@@ -0,0 +1,167 @@
+
+//variables dadas en el raffle.js
+var shoe = {
+    "model": "Sacai x Nike LDV Waffle",
+    "colour": "Varsity Blue/Del Sol/Varsity Red",
+    "code": "BV0073-400",
+    "avaliable": "07/03/19",
+    "price": "180$"
+};
+
+var raffles = {
+
+    "Antonia Milano": {
+        "logo": "https://www.soleretriever.com/wp-content/uploads/2018/04/AntoniaMilano.jpg",
+        "country": "Italy",
+        "purchase": "Online Raffle",
+        "collection": "Postage Available",
+        "Sizes": "4 to 12 US",
+        "Opens": "live",
+        "Closes": "06/03 @ 11AM CET",
+        "url": "https://www.antonia.it/164-shoes"
+    },
+
+    "END": {
+        "logo": "https://www.soleretriever.com/wp-content/uploads/2018/04/End.jpg",
+        "country": "UK",
+        "purchase": "Online Raffle",
+        "collection": "Postage Available",
+        "Sizes": "5 to 12 UK",
+        "Opens": "live",
+        "Closes": "07/03 @ 12AM GMT",
+        "url": "https://launches.endclothing.com/"
+    },
+
+    "Foot Patrol": {
+        "logo": "https://www.soleretriever.com/wp-content/uploads/2018/04/FootPatrol.png",
+        "country": "France",
+        "purchase": "Online Raffle",
+        "collection": "Collection Only",
+        "Sizes": "36.5 to 47.5 EU",
+        "Opens": "live",
+        "Closes": "04/02 @ 10AM CET",
+        "url": "https://www.footpatrol.com/customer-service/raffle-fr/"
+    },
+
+    "Holypop": {
+        "logo": "https://www.soleretriever.com/wp-content/uploads/2018/04/HolyPop.png",
+        "country": "Italy",
+        "purchase": "Online FCFS",
+        "collection": "Postage Available",
+        "Sizes": "TBC",
+        "Opens": "announced",
+        "Closes": "07/02 @ 12AM CET",
+        "url": "https://www.holypopstore.com/en/footwear"
+    },
+
+    "Offspring": {
+        "logo": "https://www.soleretriever.com/wp-content/uploads/2018/04/OffSpring.jpg",
+        "country": "UK",
+        "purchase": "Online Raffle",
+        "collection": "Collection Only",
+        "Sizes": "3.5 to 7 UK",
+        "Opens": "live",
+        "Closes": "closed",
+        "url": "https://www.offspring.co.uk/release-dates"
+    },
+
+    "SNS": {
+        "logo": "https://www.soleretriever.com/wp-content/uploads/2018/04/SNS.jpg",
+        "country": "Swe, UK, Ger, Fr",
+        "purchase": "Online Raffle",
+        "collection": "Post and Collect",
+        "Sizes": "4 to 13 US",
+        "Opens": "live",
+        "Closes": "06/03 @ 11AM CET",
+        "url": "https://www.sneakersnstuff.com/en/937/sns-raffles"
+    },
+
+    "Solebox": {
+        "logo": "https://www.soleretriever.com/wp-content/uploads/2018/04/SoleBox.jpg'git",
+        "country": "Germany",
+        "purchase": "In-Store/Online",
+        "collection": "Post and Collect",
+        "Sizes": "41 to 46 EU",
+        "Opens": "announced",
+        "Closes": "When sold out",
+        "url": "https://www.solebox.com/en/Footwear/"
+    },
+
+};
+
+
+//variables globales necesitadas
+
+
+
+//functions
+
+//funcion para crear la info del zapato
+function createShoe() {
+
+    let textBox = document.getElementById("text");
+
+    var div = document.createElement("div");
+    div.setAttribute("class", "col-md-6");
+    var title = document.createElement("h2");
+    title.innerHTML = shoe.model + "<br>";
+    div.appendChild(title);
+
+    var colour = document.createElement("h4");
+    colour.innerHTML = shoe.colour + "<br>";
+    div.appendChild(colour);
+
+    var otherText = document.createElement("p");
+    otherText.innerHTML = shoe.code + " / "+ shoe.avaliable+" / "+ shoe.price+"<br>";
+    div.appendChild(otherText);
+    textBox.appendChild(div);
+
+
+}
+
+function createRaffles(){
+for (let element of raffles ){
+    var div = document.createElement("div");
+    div.setAttribute("class", "col-md-2");
+    var image = document.createElement("image");
+    image.setAttribute("src",element.logo);
+    div.appendChild(image);
+
+    var title = document.createElement("h6");
+    title.innerHTML = element.toString() + "<br>";
+    div.appendChild(title);
+
+    var country = document.createElement("p");
+    country.innerHTML = element.country + "<br>";
+    div.appendChild(country);
+
+    var purchase = document.createElement("p");
+    purchase.innerHTML = element.purchase + "<br>";
+    div.appendChild(purchase);
+
+    var collention = document.createElement("p");
+    collention.innerHTML = element.collection + "<br>";
+    div.appendChild(collention);
+
+    var sizes = document.createElement("p");
+    sizes.innerHTML = element.sizes + "<br>";
+    div.appendChild(sizes);
+
+    var opens = document.createElement("p");
+    opens.innerHTML = "opens -"+element.opens + "<br>";
+    div.appendChild(opens);
+
+    var closes = document.createElement("p");
+    closes.innerHTML = "closes -"+element.closes + "<br>";
+    div.appendChild(closes);
+
+
+
+
+
+}
+}
+
+createShoe();
+
+
